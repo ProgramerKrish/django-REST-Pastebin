562f79a57ae7815d4ba4fc00ffa559798eb872f0
ProgramerKrish <krishnachaithanya1307@gmail.com>
2026-02-17 23:55:03 +0530

    add question&choice model with basic JSON views

---
+++ polls/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # This maps the root of 'polls/' to your index view
+    path('api/questions/', views.index),
+    path("api/<int:question_id>",views.detail),
+]
--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
+from .models import Question
 # Create your views here.
+def index(request):
+    questions=Question.objects.all()
+
+    data={
+        "quetions":[{
+            "id":q.id,
+            "question_text":q.question_text,
+            "pub_date":q.pub_date,
+        }
+        ]
+        for q in questions
+    }
+
+    return JsonResponse(data)
+
+def detail(request,question_id):
+    question=Question.objects.get(id=question_id)
+
+    data={
+        "id":question.id,
+        "question_text":question.question_text,
+        "choices":[
+            {
+                "id":c.id,
+                "choice_text":c.choice_text,
+                "votes":c.vote,
+            }
+            for c in question.choice_set.all()
+        ]
+    }
+
+    return JsonResponse(data)
