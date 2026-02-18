cb5bfea1943a278ee78490ee0ff78a440fbd0e08
ProgramerKrish <krishnachaithanya1307@gmail.com>
2026-02-18 23:55:31 +0530

    json views for vote & details

--- polls/urls.py
+++ polls/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     # This maps the root of 'polls/' to your index view
     path('api/questions/', views.index),
     path("api/<int:question_id>",views.detail),
+    path("questions/<int:question_id>/vote/",views.vote,name="vote"),
+    path("questions/<int:question_id>/vote-page/",views.vote_page),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,24 +1,26 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from .models import Question
+from .models import Question,Choice
+from django.shortcuts import get_object_or_404
+import json
 # Create your views here.
 def index(request):
-    questions=Question.objects.all()
+    questions=get_object_or_404( Question)
 
     data={
-        "quetions":[{
-            "id":q.id,
+        "questions":[{
+            "question_id":q.id,
             "question_text":q.question_text,
-            "pub_date":q.pub_date,
+            "pub_date": q.pub_date
         }
-        ]
         for q in questions
+        ]
     }
 
     return JsonResponse(data)
 
 def detail(request,question_id):
-    question=Question.objects.get(id=question_id)
+    question=get_object_or_404(Question,id=question_id)
 
     data={
         "id":question.id,
@@ -34,3 +36,76 @@ def detail(request,question_id):
     }
 
     return JsonResponse(data)
+""" def vote(request,question_id):
+    question=get_object_or_404(Question,id=question_id)
+    data={
+        "id":question_id,
+        "choices":[{"choice_id":c.id,
+                    "choice_text":c.choice_text,
+                    "vote":c.vote,
+                    }
+                    for c in question.choice_set.all()
+                    ]
+    }
+    return JsonResponse(data) """
+
+""" def vote(request,question_id):
+    if request.method != "POST":
+        return JsonResponse(
+            {"error":"only POST method allowed"},
+            status=405
+        )
+    question=get_object_or_404(Question,id=question_id)
+
+    try:
+        body=json.loads(request.body)
+        choice_id=body["choice_id"]
+    except(json.JSONDecodeError,KeyError):
+        return JsonResponse(
+            {"error":"invalid JSON or missing choice_id"},
+            status=400
+        )
+    choice=get_object_or_404(Choice,id=choice_id,question=question)
+    choice.vote+=1
+    choice.save()
+
+    return JsonResponse(
+        {
+            "message":"vote recoreded",
+            "question_text":question.id,
+            "choice_id":choice.id,
+            "total_votes":choice.vote
+        }
+    )
+ """
+def vote(request,question_id):
+    if request.method != "POST":
+        return JsonResponse(
+            {"error":"only POST method allowed"},
+            status=405
+        )
+    question=get_object_or_404(Question,id=question_id)
+
+    try:
+        choice_id=request.POST["choice"]
+    except(KeyError):
+        return JsonResponse(
+            {"error":"invalid JSON or missing choice_id"},
+            status=400
+        )
+    choice=get_object_or_404(Choice,id=choice_id,question=question)
+    choice.vote+=1
+    choice.save()
+
+    return JsonResponse(
+        {
+            "message":"vote recoreded",
+            "question_text":question.question_text,
+            "choice_id":choice.id,
+            "total_votes":choice.vote
+        }
+    )
+
+def vote_page(request,question_id):
+    question=get_object_or_404(Question,id=question_id)
+    return(render(request,"polls/votes_test.html",{"question":question}))
