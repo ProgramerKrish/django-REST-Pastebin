3b7cfd3dee1ec16831baaae4e6cb58f8cab2fa59
Krish <167979046+ProgramerKrish@users.noreply.github.com>
2026-03-02 23:51:50 +0530

    Update URL patterns to use class-based views

--- snippets/urls.py
+++ snippets/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from snippets import views 
 from rest_framework.urlpatterns import format_suffix_patterns
 urlpatterns=[
-    path("snippets/",views.snippet_list),
-    path("snippets/<int:pk>/",views.snippet_detail),
+    path("snippets/",views.snippet_list.as_view()),
+    path("snippets/<int:pk>/",views.snippet_detail.as_view()),
 ]
 
 urlpatterns=format_suffix_patterns(urlpatterns)
